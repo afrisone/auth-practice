1f0ebc0f9689cd62024291d0664a152ccf1c6e70
Alicia Frisone <a.frisone54@gmail.com>
2023-07-23 19:38:46 -0600

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import config from "config";
+import express from "express";
 
+const port = config.get("port");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({});
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
